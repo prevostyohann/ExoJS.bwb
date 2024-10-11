f8acd2352df4f51f241e074c1679d1844b9adec9
prevostyohann <prevost.yohann@yahoo.fr>
2024-10-11 09:32:47 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let monCanard = "donald";
+const nombreDePate = 2;
+const nombreDAile = 2;
+
+function canard (nombreDePate, nombreDAile) {
+let maCanette = "daisy";
+let mesCanards = maCanette += monCanard;
+console.log(mesCanards);
+}
